3cd614a467f6c721f90bb8710f57a7e23b3a70c6
Rohan <rohansethi1999430@gmail.com>
2021-07-18 22:54:03 +0530

    Created get and post api and also schema

--- server.js
+++ server.js
@@ -2,15 +2,25 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Cors from 'cors'
 
 // app config
 const app=express();
 const port= process.env.PORT || 9000
+
 //middleware
+app.use(express.json());
 
 
 //DB config
+const connection_url='mongodb+srv://admin:rs.9897637879@cluster0.q5kco.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 
+mongoose.connect(connection_url,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 //?????
 
@@ -20,6 +30,31 @@ app.get('/',(req,res)=>{
     res.status(200).send("Hello from Rohan's Server");
 })
 
+app.post('/messages/new',(req,res)=>{
+    const dbMessage=req.body;
+
+    Messages.create(dbMessage,(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    })
+})
+
+app.get('/messages/sync',(req,res)=>{
+    
+
+    Messages.find((err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    })
+})
 //listen
 
 app.listen(port,()=>{console.log(`Listening on port : ${port}`)})
